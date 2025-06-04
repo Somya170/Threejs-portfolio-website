3aa8623d9325e36754e58674fbbeb3eb31494882
Somya Jaiswal <somya.jaiswal1218@gmail.com>
2025-06-05 01:24:37 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Tilt from "react-parallax-tilt"; // ✅ replaced package
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt
+    tiltMaxAngleX={25}
+    tiltMaxAngleY={25}
+    glareEnable={true}
+    glareMaxOpacity={0.2}
+    scale={1.05}
+    transitionSpeed={450}
+    className="xs:w-[250px] w-full"
+  >
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Experienced in Artificial Intelligence, Machine Learning, Java (Core & Advanced), Python, and SQL, with practical knowledge of Spring Boot, Hibernate, J2EE, and RESTful APIs. My strength is building scalable backend systems and implementing AI/ML methods to real-world problems.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
